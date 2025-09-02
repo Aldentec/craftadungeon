ae108d51e846f764238f203108e4e7d91170c756
Aldentec <65435340+Aldentec@users.noreply.github.com>
2025-09-02 04:48:39 -0700

    SEO optimiziation take 1

--- .history/src/index_20250831133353.js
+++ .history/src/index_20250831133353.js
@@ -11,8 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// Measure performance and send to analytics
-reportWebVitals((metric) => {
-  // Send to Google Analytics or other analytics service
-  console.log(metric);
-});
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
